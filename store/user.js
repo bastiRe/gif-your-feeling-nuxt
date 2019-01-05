60b566bb6f38af3323eeab7b450c0cc631818fc4
Sebastian Rehm <rehmsebastian@gmail.com>
2019-01-05 20:12:49 +0100

    Add persistence of user info

--- store/user.js
+++ store/user.js
@@ -1,13 +1,23 @@
 import shortid from 'shortid'
 
-export const state = () => ({
-  name: null,
-  id: null
-})
+export const state = () => {
+  let user = {
+    name: null,
+    id: null
+  }
+
+  try {
+    const storedUser = localStorage.getItem('user')
+    user = JSON.parse(storedUser)
+  } catch (e) {}
+
+  return user
+}
 
 export const mutations = {
   setName(state, name) {
     state.name = name
     state.id = shortid()
+    localStorage.setItem('user', JSON.stringify(state))
   }
 }
